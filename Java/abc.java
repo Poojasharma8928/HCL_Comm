04f380cc7b74cb76ccdda596bfefdad6e4520178
pooja sharma <poojasharma8928@gmail.com>
2022-08-09 12:37:03 +0530

    Java Practice Code

---
+++ Java/abc.java
@@ -0,0 +1,15 @@
+
+public class Company{  
+		 float salary=40000;  
+		}  
+		class Employee extends Company{  
+		 int bonus=10000;  
+class main{
+		 public static void main(String args[]){  
+		   Employee p=new Employee();  
+		   System.out.println("Programmer salary is:"+p.salary);  
+		   System.out.println("Bonus of Programmer is:"+p.bonus);  
+		}  
+		}  
+		}
+
